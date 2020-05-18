172a23be7b0367e7f3a2f973c5c155a50222fd4e
Jojoe Sagoe <jojoe.sagoe@yahoo.com>
2020-05-17 22:49:39 -0700

    Create main.cpp

---
+++ SinglyLinkedList/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include "mylinkedList.cpp"
+
+using namespace std;
+
+
+int main()
+{
+    vector<int> vec{23,12,4,6,2,8,19,10};
+
+    LinkedList ls;
+    ls.printList();
+    cout << "current size: " << ls.getSize() < <endl;
+    ls.addNodeAtIndex(5);
+    cout << "current size: " << ls.getSize() << endl;
+    ls.printList();
+    ls.addToHead(6);
+    cout << "current size: " << ls.getSize() << endl;
+    ls.addToTail(8);
+    cout << "current size: " << ls.getSize() << endl;
+    ls.printList();
+    for (int x:vec){
+        ls.addToTail(x);
+    }
+    ls.printList();
+    cout << "size: " << ls.getSize() << endl;
+    cout << ls.deleteFromTail() << endl;
+    cout<<ls.deleteFromHead()<<endl;
+    ls.printList();
+    ls.addNodeAtIndex(17, 4);
+    ls.printList();
+    ls.FindAndDeleteNode(10);
+    ls.printList();
+    cout << "current size: " << ls.getSize() << endl;
+    return 0;
+}
