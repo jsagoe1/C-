5ebada519ca1222b9b844e8ca7b417ad5a40a7a7
Jojoe Sagoe <jojoe.sagoe@yahoo.com>
2020-05-17 22:48:40 -0700

    Create mylinkedList.h

---
+++ SinglyLinkedList/mylinkedList.h
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Node {
+    public:
+    Node(){
+        next = 0;
+    }
+    Node(int v, Node* n = 0) {
+        val = v; next = n;
+    }
+
+    int val;
+    Node* next;
+};
+
+class LinkedList {
+    public:
+        LinkedList()
+        {
+            head = tail = 0;
+        }
+        void addNodeAtIndex(int, int);
+        bool isEmpty();
+        void addToHead(int);
+        void addToTail(int);
+        int deleteFromHead();
+        int deleteFromTail();
+        void FindAndDeleteNode(int);
+        bool isInList(int);
+        int getSize();
+        void printList();
+    private:
+        Node *head;
+        Node *tail;
+
+};
