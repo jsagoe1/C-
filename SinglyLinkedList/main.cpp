b8c5c8060193ffcb1e6a5fda3926ef9ceab97481
Jojoe Sagoe <jojoe.sagoe@yahoo.com>
2020-09-10 01:21:59 -0700

    Update main.cpp

--- SinglyLinkedList/main.cpp
+++ SinglyLinkedList/main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
-#include <string>
-#include "LinkedList.h"
+#include "SinglyLinkedList.cpp"
+#include "SinglyLinkedList.h"
 
 using namespace std;
 
-
 int main() {
-	SinglyLinkedList list1(0);
-	int arr[5] = { 1,2,3,4,5 };
-	for (int i = 0; i < 5; i++) {
-		list1.append(arr[i]);
+	SinglyLinkedList<int> s;
+	SinglyLinkedList<int> q;
+	for (int i = 0; i < 5; i += 1) {
+		s.insertFront(i);
 	}
-	list1.insertAfterNode(10, 9);
-	list1.printList();
-	list1.deleteNode(9);
-	list1.printList();
+	cout << "s:" << s << endl;
+	s.insertAtIndex(4, 10);
+	cout << "s:" << s << endl;
+	s.erase(2);
+	cout << "s:" << s << endl;
+	s.sort();
+	cout << "s:" << s << endl;
+	s.erase(4);
+	q.assign(s);
+	cout << "s:" << s << endl;
+	cout << "q:" << q << endl;
+	s.erase(10);
+	cout << "s:" << s << endl;
+	cout << "q:" << q << endl;
 	return 0;
 }
